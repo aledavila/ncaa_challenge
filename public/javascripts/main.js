028547604e5b8f77927ab18874190fc510c03007
Alessandra Davila <davilaaguilar23@gmail.com>
2016-02-09 11:49:19 -0600

    adds linking accounts and shows data in page

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,7 +1,11 @@
 $(function(){
 
+  var appendCurrentGames = function(currentGames) {
+
+  }
+
   $.ajax({
-    url: 'https://www.kimonolabs.com/api/9w66kn6i',
+    url: 'https://www.kimonolabs.com/api/2mzdpsn2',
     apikey: 'NCAA_API_KEY',
     method: 'GET',
     dataType: 'jsonp',
@@ -10,7 +14,27 @@ $(function(){
     }
   })
   .done(function(data) {
-    console.log(data);
+    var currentGames = data['results']['collection1'];
+
+    $.each(currentGames, function(_, value) {
+      $('.list').append('<div class="item"><div class="header"><span class="away">' + value.awayTeam.text + '</span> <span class="notbold">v</span> <span class="home">' + value.homeTeam.text + '</span></div>');
+    });
+
+    // // Take results and output to the page
+    // for (var i = 0; i < currentGames.length; i++) {
+    //   // console.log('Game: ', currentGames[i]);
+
+    //   var games = '<div class="item"><div class="header">' + currentGames[i].away + '</h4>' +
+    //     '<img src="' + movies[i].Poster + '">' +
+    //     '</li>';
+
+    //     // <div class="item">
+    //     //   <div class="header">New York City</div>
+    //     //   A lovely city
+    //     // </div>
+
+    //   $('.ui.list').append(games);
+    // }
   });
 
 });
